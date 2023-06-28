8268feeb8897200624ae3c2d469baf542386afb3
이치연 <leechi2228@gamil.com>
2023-06-28 09:03:38 +0900

    Add layout & css & js & img

--- js/header.js
+++ js/header.js
@@ -1,8 +1,12 @@
-let profileImg = document.querySelector('.img-profile')
+let profileImg = document.querySelector('.img-profile__img')
 let dropdownMenu = document.querySelector('.header__profile__dropdown')
-
+let main = document.querySelector('.main')
 
 
 profileImg.addEventListener('click', () => {
     dropdownMenu.classList.toggle('hidden')
+})
+
+main.addEventListener('mouseover', () => {
+    dropdownMenu.classList.add('hidden')
 })
